22073e6dd59ae58af6aed0b1e77c1d678c90777a
Facundo Zapata <facundozapata41@gmail.com>
2024-06-02 20:52:36 -0300

    se agrega boton para navegar a fragmento search result + importacion de imagenes al proyecto

--- app/src/main/java/com/example/tp3_fickleflight_grupo7/fragments/SearchResultFragment.kt
+++ app/src/main/java/com/example/tp3_fickleflight_grupo7/fragments/SearchResultFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import com.example.tp3_fickleflight_grupo7.R
 
-class ExploreFragment : Fragment() {
-
+class SearchResultFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -19,7 +21,7 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        return inflater.inflate(R.layout.fragment_search_result, container, false)
     }
 
 }
