765759748f1e00674dc80385a0888c3b79bb4c44
Facundo Zapata <facundozapata41@gmail.com>
2024-06-04 01:17:36 -0300

    commit parcial implementacion drawer todo roto

--- app/src/main/java/com/example/tp3_fickleflight_grupo7/activities/MainActivity.kt
+++ app/src/main/java/com/example/tp3_fickleflight_grupo7/activities/MainActivity.kt
@@ -3,28 +3,60 @@ package com.example.tp3_fickleflight_grupo7.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.tp3_fickleflight_grupo7.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavView : BottomNavigationView
     private lateinit var navHostFragment : NavHostFragment
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var toolbar : Toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        bottomNavView = findViewById(R.id.bottom_bar)
-        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
-
         toolbar= findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+
+        bottomNavView = findViewById(R.id.bottom_navigation)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav, R.id.navigation_dashboard, R.id.navigation_notifications),
+            drawerLayout
+        )
+
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
