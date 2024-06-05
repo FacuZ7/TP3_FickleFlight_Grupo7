a1f7fb17f1afb56e548f3600e21a3b0fe11b7f72
Facundo Zapata <facundozapata41@gmail.com>
2024-06-05 01:09:00 -0300

    implementacion de modo oscuro y guardar en shared preferences

--- app/src/main/java/com/example/tp3_fickleflight_grupo7/activities/SplashScreen.kt
+++ app/src/main/java/com/example/tp3_fickleflight_grupo7/activities/SplashScreen.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.preference.PreferenceManager
 import com.example.tp3_fickleflight_grupo7.MainActivity
 import com.example.tp3_fickleflight_grupo7.R
 
@@ -12,12 +15,20 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val preferencias = PreferenceManager.getDefaultSharedPreferences(this)
+        val nightMode = preferencias.getBoolean("night_mode",false)
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (nightMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 5000)
+
     }
+
 }
